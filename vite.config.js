603a65b5d30512e8dc034f24a18ab1c838afb29a
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-08-07 14:55:01 +0300

    aa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Arabic_Servicsc_Center/"
+  base:"/Arabic_Servicsc_Center/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // قسم الحزم لتقليل الحجم
+          vendor: ['react', 'react-dom']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // زيادة الحد الأقصى لحجم التحذير
+  }
 })
